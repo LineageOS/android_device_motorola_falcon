da8ed71330ba3da284c4e8814b104eec0717336d
luca stracquadanio <lucastracquadanio@gmail.com>
2017-01-31 02:27:14 +0100

    Update and rename lineage.mk to du.mk

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := lineage_falcon
+PRODUCT_NAME := du_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
